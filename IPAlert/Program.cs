b4dbce0051daec2b671539ac972f6179a9c713f3
Eisen <eisen8@users.noreply.github.com>
2025-03-16 02:25:59 -0700

    Enforcing that only a single instance runs at a time

--- IPAlert/Program.cs
+++ IPAlert/Program.cs
@@ -14,6 +14,7 @@ namespace IPAlert
     {
         private static IPAlert? _ipAlert;
         private static IContainer? _container;
+        private static Mutex? _mutex; // Used to enforce single instance of app running
 
         /// <summary>
         ///  The boilerplate Main entry point for the application.
@@ -25,6 +26,13 @@ namespace IPAlert
             logger.Info("Starting IPAlert Application");
             try
             {
+                // Enforce single instance
+                if (!checkIsNewInstance(logger))
+                {
+                    logger.Info("Another instance of IPAlert is already running, exiting.");
+                    return;
+                }
+
                 // Localization
                 Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
                 Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
@@ -53,6 +61,11 @@ namespace IPAlert
             }
         }
 
+        /// <summary>
+        /// Configures the dependency injection container
+        /// </summary>
+        /// <param name="logger">The logger</param>
+        /// <returns>The DI container</returns>
         private static IContainer dependencyConfiguration(Logger logger)
         {
             FileSystem fs = new FileSystem();
@@ -66,6 +79,32 @@ namespace IPAlert
             builder.RegisterType<IPAlert>().AsSelf().SingleInstance();
 
             return builder.Build();
+        }
+
+
+        /// <summary>
+        /// Checks if the system is already running an instance of the application.
+        /// </summary>
+        /// <param name="logger">The logger</param>
+        /// <returns>Returns true if this is a new instance. False if there is another instance running.</returns>
+        private static bool checkIsNewInstance(Logger logger)
+        {
+            string mutexName = "Global\\IPAlertMutex"; // Global mutexs are system-wide, so it is held across applications
+            bool isNewInstance;
+            try
+            {
+                _mutex = new Mutex(true, mutexName, out isNewInstance);
+                return isNewInstance;
+
+            }
+            catch (AbandonedMutexException) 
+            {
+                // This occurs when previous application was forcibly terminated and mutex was abandoned
+                logger.Info("Abandoned mutex found, creating new instance");
+                // create a new mutex
+                _mutex = new Mutex(true, mutexName);
+                return true;
+            }
         }
     }
 }
